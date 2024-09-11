fb8ed076e88f87c4a3338ec1a727419d964a7b8d
MangeshDave <mangesh.dave05@gmail.com>
2024-09-11 08:14:44 +0530

    Adding files

---
+++ src/june/ex_04092024/Lab003.java
@@ -0,0 +1,10 @@
+package june.ex_04092024;
+
+public class Lab003 {
+    public static void main(String[] Mangesh){
+        System.out.println(2+2);
+        System.out.println(2-2);
+        System.out.println(2*2);
+        System.out.println(2/2);
+    }
+}
---
+++ src/june/ex_04092024/Lab004.java
@@ -0,0 +1,4 @@
+package june.ex_04092024;
+
+public class Lab004 {
+}
